2cd8c0c26e1d25947c2e3c9ea103ce53e82d096e
Slaven Rezic <slaven@rezic.de>
2000-08-09 07:06:43 +0000

    WidgetInfo etc.

--- lib/Tk/WidgetDump.pm
+++ lib/Tk/WidgetDump.pm
@@ -2,7 +2,7 @@
 # -*- perl -*-
 
 #
-# $Id: WidgetDump.pm,v 1.5 2000/01/24 00:33:41 eserte Exp $
+# $Id: WidgetDump.pm,v 1.6 2000/08/09 07:06:43 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 1999 Slaven Rezic. All rights reserved.
@@ -40,7 +40,8 @@ sub WidgetDump {
 		       -exportselection => 1,
 		       -takefocus => 1,
 		       -command => sub {
-			   $hl->info('data', $_[0])->_WD_Flash;
+			   my $sw = $hl->info('data', $_[0]);
+			   Tk::WidgetDump::_show_widget($t, $sw);
 		       },
 		      )->pack(-fill => 'both', -expand => 1);
     $hl->focus;
@@ -81,27 +82,42 @@ sub WidgetDump {
 
 sub _WD_Flash {
     my $w = shift;
+    my $wd = shift;
     eval {
+	# Wenn ein Widget während eines Flashs nochmal ausgewählt wird,
+	# muss es erst einmal zurückgesetzt werden.
+	if (defined $wd->{OldRepeat}) {
+	    $wd->{OldRepeat}->cancel;
+	    if (defined $wd->{OldBg}) {
+		$wd->{OldWidget}->configure(-background => $wd->{OldBg});
+	    }
+	}
+
 	my $old_bg = $w->cget(-background);
-	$w->configure(-background => "red");
+	# leicht verzögern, damit -background nicht vom Blinken verfälscht wird
+	$w->after(10, sub { $w->configure(-background => "red") });
 	$w->raise;
 	my $i = 0;
-	# XXX wenn ein widget während eines flashs nochmal ausgewählt wird,
-	# dann bleibt das widget rot...
+
 	my $flash_rep;
 	$flash_rep = $w->repeat
 	  (500,
 	   sub {
-	       if ($i % 2 == 1) {
+	       if ($i % 2 == 0) {
 		   $w->configure(-background => "red");
 	       } else {
 		   $w->configure(-background => $old_bg);
 	       }
 	       if (++$i > 8) {
 		   $flash_rep->cancel;
+		   undef $wd->{OldRepeat};
 		   $w->configure(-background => $old_bg);
 	       }
 	   });
+
+	$wd->{OldWidget} = $w;
+	$wd->{OldBg}     = $old_bg;
+        $wd->{OldRepeat} = $flash_rep;
     };
     warn $@ if $@;
 }
@@ -118,8 +134,104 @@ sub _WD_Size {
     $size;
 }
 
+sub _WD_WidgetInfo {
+    my $w = shift;
+    my $wd = shift;
+
+    my $wi = Tk::WidgetDump::_get_widget_info_window($wd);
+    $wi->title("Widget Info for " . $w);
+
+    my $txt = $wi->Subwidget("Information");
+    $txt->delete("1.0", "end");
+
+    $txt->insert("end", "Configuration:\n\n");
+    foreach my $c ($w->configure) {
+	$txt->insert("end",
+		     join("\t", map { !defined $_ ? "<undef>" : $_ } @$c)
+		     . "\n");
+    }
+    $txt->insert("end", "\n");
+
+    my $insert_method = sub {
+	my($meth, $label) = @_;
+	$label = $meth if !defined $label;
+	$txt->insert("end", "$label:\t" . $w->$meth() . "\n");
+    };
+
+    $insert_method->("name", "Name");
+    $insert_method->("PathName");
+    $insert_method->("Class");
+
+    if (defined $w->parent) {
+	$txt->insert("end", "Parent:\t" . $w->parent,
+		     ["widgetlink", "href-" . $w->parent], "\n");
+	$Tk::WidgetDump::ref2widget{$w->parent} = $w->parent;
+    }
+
+    if (defined $w->toplevel) {
+	$txt->insert("end", "Toplevel:\t" . $w->toplevel,
+		     ["widgetlink", "href-" . $w->toplevel],
+		     "\n");
+	$Tk::WidgetDump::ref2widget{$w->toplevel} = $w->toplevel;
+    }
+
+    if (defined $w->MainWindow) {
+	$txt->insert("end", "MainWindow:\t" . $w->MainWindow,
+		     ["widgetlink", "href-" . $w->MainWindow],
+		     "\n");
+	$Tk::WidgetDump::ref2widget{$w->MainWindow} = $w->MainWindow;
+    }
+
+    $insert_method->("manager", "GeomManager");
+    $insert_method->("geometry");
+    $insert_method->("rootx");
+    $insert_method->("rooty");
+    $insert_method->("vrootx");
+    $insert_method->("vrooty");
+    $insert_method->("x");
+    $insert_method->("y");
+    $insert_method->("width");
+    $insert_method->("height");
+    $insert_method->("reqwidth");
+    $insert_method->("reqheight");
+    $insert_method->("id");
+    $insert_method->("ismapped");
+    $insert_method->("viewable");
+
+    $txt->insert("end", "\nServer:\n");
+    $insert_method->("server", "    id");
+    $insert_method->("visual", "    visual");
+#XXX dokumentiert, aber nicht vorhanden?!
+#    $insert_method->("visualid", "    visualid");
+    $insert_method->("visualsavailable", "    visualsavailable");
+
+    $txt->insert("end", "\nRoot window:\n");
+    $insert_method->("vrootwidth", "    vrootwidth");
+    $insert_method->("vrootheight", "    vrootheight");
+
+    $txt->insert("end", "\nScreen:\n");
+    $insert_method->("screen", "    id");
+    $insert_method->("screencells", "    cells");
+    $insert_method->("screenwidth", "    width");
+    $insert_method->("screenheight", "    height");
+    $insert_method->("screenmmwidth", "    width (mm)");
+    $insert_method->("screenmmheight", "    height (mm)");
+    $insert_method->("screenvisual", "    visual");
+    
+    $txt->insert("end", "\nColor map:\n");
+    $insert_method->("cells", "    cells");
+    $insert_method->("colormapfull", "    full");
+    $insert_method->("depth", "    depth");
+}
+
 package Tk::WidgetDump;
 use File::Basename;
+
+sub _show_widget {
+    my($wd, $w) = @_;
+    $w->_WD_Flash($wd);
+    $w->_WD_WidgetInfo($wd);
+}
 
 sub _insert_wd {
     my($hl, $top, $par) = @_;
@@ -172,6 +284,86 @@ sub _crop {
 	$txt;
     }
 }
+
+sub _get_widget_info_window {
+    my $wd = shift;
+
+    my $wi = $wd->Subwidget("WidgetInfo");
+
+    if ($wi and Tk::Exists($wi)) {
+	$wi->raise;
+	return $wi;
+    }
+
+    $wi = $wd->Component(Toplevel => "WidgetInfo");
+    $wi->title("Widget Info");
+
+    my $txt = $wi->Scrolled("Text",
+			    -tabs => [map { (5*$_) . "c" } (1 .. 8)],
+			    -wrap => "none",
+			   )->pack(-expand => 1, -fill => "both");
+
+    $txt->tagConfigure(qw/widgetlink -underline 1/);
+    $txt->tagConfigure(qw/hot        -foreground red/);
+    $txt->tagBind(qw/widgetlink <ButtonRelease-1>/ => sub {
+	my($text) = @_;
+
+	my $index = $text->index('current');
+	my @tags = $txt->tagNames($index);
+	my $i = _lsearch('href\-.*', @tags);
+	return if $i < 0;
+	my($href) = $tags[$i] =~ /href-(.*)/;
+	my $widget = $ref2widget{$href};
+	_show_widget($wd, $widget);
+    });
+    my $last_line = '';
+    $txt->tagBind(qw/widgetlink <Enter>/ => sub {
+	my($text) = @_;
+	my $e = $text->XEvent;
+	my($x, $y) = ($e->x, $e->y);
+	$last_line = $text->index("\@$x,$y linestart");
+	$text->tagAdd('hot', $last_line, "$last_line lineend");
+	$text->configure(qw/-cursor hand2/);
+    });
+    $txt->tagBind(qw/widgetlink <Leave>/ => sub {
+	my($text) = @_;
+	$text->tagRemove(qw/hot 1.0 end/);
+	$text->configure(qw/-cursor xterm/);
+    });
+    $txt->tagBind(qw/widgetlink <Motion>/ => sub {
+	my($text) = @_;
+	my $e = $text->XEvent;
+	my($x, $y) = ($e->x, $e->y);
+	my $new_line = $text->index("\@$x,$y linestart");
+	if ($new_line ne $last_line) {
+	    $text->tagRemove(qw/hot 1.0 end/);
+	    $last_line = $new_line;
+	    $text->tagAdd('hot', $last_line, "$last_line lineend");
+	}
+    });
+
+    $wi->Advertise("Information" => $txt);
+    $wi->Component(Button => "Close",
+		   -text => "Close",
+		   -command => sub { $wi->destroy })->pack;
+
+    $wi;
+}
+
+sub _lsearch {
+
+    # Search the list using the supplied regular expression and return it's
+    # ordinal, or -1 if not found.
+
+    my($regexp, @list) = @_;
+    my($i);
+
+    for ($i=0; $i<=$#list; $i++) {
+        return $i if $list[$i] =~ /$regexp/;
+    }
+    return -1;
+
+} # end lsearch
 
 # XXX weitermachen
 # die Idee: die gesamten Konfigurationsdaten aller Widgets per configure
