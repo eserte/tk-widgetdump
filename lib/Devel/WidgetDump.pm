956d971effc212982ce072890c86973b59b3f735
Slaven Rezic <slaven@rezic.de>
2001-02-10 20:32:09 +0000

    *** empty log message ***

--- lib/Devel/WidgetDump.pm
+++ lib/Devel/WidgetDump.pm
@@ -1,7 +1,7 @@
 # -*- perl -*-
 
 #
-# $Id: WidgetDump.pm,v 1.1 2000/10/07 01:46:08 eserte Exp $
+# $Id: WidgetDump.pm,v 1.2 2001/02/10 20:32:18 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2000 Slaven Rezic. All rights reserved.
@@ -13,25 +13,49 @@
 #
 
 package Devel::WidgetDump;
-use Tk::WidgetDump;
 
-$seen = 0;
+#  require Tk;
+#  require Tk::WidgetDump;
 
-sub DB::DB {
-warn caller;
-    if (!$seen && defined &Tk::MainWindow::Existing) {
-	my $top = (Tk::MainWindow::Existing())[0];
-	if (Tk::Exists($top)) {
-	    $top->bind("<Control-d>" => sub { $top->WidgetDump });
-	    $top->bind("<Control-p>" => sub {
-			   require Config;
-			   my $perldir = $Config::Config{'scriptdir'};
-			   require "$perldir/ptksh";
-		       });
-	    $seen++;
-	}
-    }
+#  sub Tk::MainLoop
+#  {
+#   my $mw = (Tk::MainWindow::Existing())[0];
+#   if (Tk::Exists($mw)) {
+#       $mw->WidgetDump;
+#   }
+
+#   unless ($inMainLoop)
+#    {
+#     local $inMainLoop = 1;
+#     while (Tk::MainWindow->Count)
+#      {
+#       DoOneEvent(0);
+#      }
+#    }
+#  }
+
+{
+    package DB;
+    sub DB { } #warn "@_ "  . caller . "\n" }
 }
+
+#$seen = 0;
+#
+#  sub DB::DB {
+#  warn caller;
+#      if (!$seen && defined &Tk::MainWindow::Existing) {
+#  	my $top = (Tk::MainWindow::Existing())[0];
+#  	if (Tk::Exists($top)) {
+#  	    $top->bind("<Control-d>" => sub { $top->WidgetDump });
+#  	    $top->bind("<Control-p>" => sub {
+#  			   require Config;
+#  			   my $perldir = $Config::Config{'scriptdir'};
+#  			   require "$perldir/ptksh";
+#  		       });
+#  	    $seen++;
+#  	}
+#      }
+#  }
 
 
 1;
