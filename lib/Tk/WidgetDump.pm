8e03454b7c9414c476df73105288f818eef977bc
Slaven Rezic <slaven@rezic.de>
2008-01-23 21:50:47 +0000

    * copyright year

--- lib/Tk/WidgetDump.pm
+++ lib/Tk/WidgetDump.pm
@@ -2,10 +2,10 @@
 # -*- perl -*-
 
 #
-# $Id: WidgetDump.pm,v 1.36 2008/01/23 21:50:28 eserte Exp $
+# $Id: WidgetDump.pm,v 1.37 2008/01/23 21:50:47 eserte Exp $
 # Author: Slaven Rezic
 #
-# Copyright (C) 1999-2007 Slaven Rezic. All rights reserved.
+# Copyright (C) 1999-2008 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -17,7 +17,7 @@ package Tk::WidgetDump;
 use vars qw($VERSION);
 use strict;
 
-$VERSION = sprintf("%d.%02d", q$Revision: 1.36 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.37 $ =~ /(\d+)\.(\d+)/);
 
 package # hide from CPAN indexer
   Tk::Widget;
